6d6fcdb5fcedbb2c6692c2cd03668d77385bae7a
adison <adison.soares7@gmail.com>
2023-05-11 15:02:25 -0300

    Servlet_2

---
+++ Servlet_2/src/main/java/br/com/alura/gerenciador/servlet/AlteraEmpresaServlet.java
@@ -0,0 +1,51 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AlteraEmpresaServlet
+ */
+@WebServlet("/alteraEmpresa")
+public class AlteraEmpresaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		
+		Date dataAbertura = null;	
+		Banco banco = new Banco();
+		Empresa empresa = new Empresa();
+		
+		
+		String nomeEmpresa = request.getParameter("nome");
+		String dataEmpresa = request.getParameter("data");
+		String idEmpresa = request.getParameter("id");
+		
+		Integer id = Integer.valueOf(idEmpresa);
+		
+		
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+			dataAbertura = sdf.parse(dataEmpresa);
+		} catch (ParseException e) {
+			throw new ServletException(e);
+		}
+		
+		empresa = banco.buscaEmpresaPelaId(id);
+		empresa.setNome(nomeEmpresa);
+		empresa.setDataAbertura(dataAbertura);
+		
+		response.sendRedirect("listaEmpresas");
+		
+	}
+
+}
---
+++ Servlet_2/src/main/java/br/com/alura/gerenciador/servlet/OlaSistemaServlet.java
@@ -0,0 +1,27 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/oi")
+public class OlaSistemaServlet extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse response) throws IOException{
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Olá sistema servlet!!");
+		out.println("</body>");
+		out.println("</html>");
+		
+		System.out.println("O servlet OlaSistemaServlet foi chamado!");
+	}
+
+}
