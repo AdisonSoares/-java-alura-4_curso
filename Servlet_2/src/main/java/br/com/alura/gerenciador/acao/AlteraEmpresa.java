9120f2763375aba9414a2ed417faaa472e8d7bce
adison <adison.soares7@gmail.com>
2023-05-17 09:36:37 -0300

    Servlet_2

--- Servlet_2/src/main/java/br/com/alura/gerenciador/acao/AlteraEmpresa.java
+++ Servlet_2/src/main/java/br/com/alura/gerenciador/acao/AlteraEmpresa.java
@@ -1,4 +1,4 @@
-package br.com.alura.gerenciador.servlet;
+package br.com.alura.gerenciador.acao;
 
 import java.io.IOException;
 import java.text.ParseException;
@@ -6,20 +6,18 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class AlteraEmpresaServlet
- */
-@WebServlet("/alteraEmpresa")
-public class AlteraEmpresaServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+import br.com.alura.gerenciador.modelo.Banco;
+import br.com.alura.gerenciador.modelo.Empresa;
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+public class AlteraEmpresa {
+
+	public void executa(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
+		
+		System.out.println("Alterando empresa!");
 		
 		Date dataAbertura = null;	
 		Banco banco = new Banco();
@@ -44,8 +42,7 @@ public class AlteraEmpresaServlet extends HttpServlet {
 		empresa.setNome(nomeEmpresa);
 		empresa.setDataAbertura(dataAbertura);
 		
-		response.sendRedirect("listaEmpresas");
-		
+		response.sendRedirect("entrada?acao=ListaEmpresa");
 	}
 
 }
