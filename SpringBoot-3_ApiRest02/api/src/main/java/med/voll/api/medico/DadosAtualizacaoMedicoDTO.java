92c5aa954ae6ab3a82e3eb4022b34016c23a835b
adison <adison.soares7@gmail.com>
2023-06-11 13:29:45 -0300

    SpringBoot-3_ApiRest02

---
+++ SpringBoot-3_ApiRest02/api/src/main/java/med/voll/api/medico/DadosAtualizacaoMedicoDTO.java
@@ -0,0 +1,17 @@
+package med.voll.api.medico;
+
+import jakarta.validation.constraints.NotNull;
+import med.voll.api.endereco.DadosEnderecoDTO;
+
+public record DadosAtualizacaoMedicoDTO(
+		
+		@NotNull
+		Long id, 
+		
+		String nome, 
+		
+		String telefone, 
+		
+		DadosEnderecoDTO endereco) {
+
+}
---
+++ SpringBoot-3_ApiRest02/api/src/main/java/med/voll/api/medico/Medico.java
@@ -0,0 +1,71 @@
+package med.voll.api.medico;
+
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import med.voll.api.endereco.Endereco;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Table(name = "medicos")
+@Entity(name = "Medico")
+public class Medico {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Enumerated(EnumType.STRING)
+	private Especialidade especialidade;
+
+	@Embedded
+	private Endereco endereco;
+
+	private String nome;
+	private String email;
+	private String telefone;
+	private String crm;
+	private Boolean ativo;
+	
+
+	public Medico(DadosCadastroMedicoDTO dados) {
+		this.ativo = true;
+		this.nome = dados.nome();
+		this.email = dados.email();
+		this.telefone = dados.telefone();
+		this.crm = dados.crm();
+		this.especialidade = dados.especialidade();
+		this.endereco = new Endereco(dados.endereco());
+
+	}
+
+	public void atualizarInformacoes(@Valid DadosAtualizacaoMedicoDTO dados) {
+		if(dados.nome() != null) {
+			this.nome = dados.nome();
+		}
+		if(dados.telefone() != null) {
+			this.telefone = dados.telefone();
+		}
+		if(dados.endereco() != null) {
+			this.endereco.atualizarInformacoes(dados.endereco());
+		}
+	}
+
+	public void excluir() {
+		this.ativo = false;
+	}
+}
