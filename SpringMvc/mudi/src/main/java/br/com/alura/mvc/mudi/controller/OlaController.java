01db61df6b235646b94b2f03e432cbc5f9fa0016
adison <adison.soares7@gmail.com>
2023-07-14 16:40:17 -0300

    SpringMvc

---
+++ SpringMvc/mudi/src/main/java/br/com/alura/mvc/mudi/controller/OlaController.java
@@ -0,0 +1,14 @@
+package br.com.alura.mvc.mudi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class OlaController {
+    @GetMapping("/ola")
+    public String ola(Model model){
+        model.addAttribute("nome", " Mundo");
+        return "ola";
+    }
+}
